80f9c38e473acec121674157c95970a188e0f0a9
Jason <itzimas@scidrones.com>
2026-06-18 15:07:08 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import requests
+
+response = requests.get("https://official-joke-api.appspot.com/random_joke")
+joke = response.json()
+print(f"Setup: {joke['setup']}")
+print(f"Punchline: {joke['punchline']}")
