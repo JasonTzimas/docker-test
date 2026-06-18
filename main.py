f467edab8bd10a37f948ed3e1ba89c6afe5fb802
Jason <itzimas@scidrones.com>
2026-06-18 15:45:37 +0300

    Add a print in main

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ response = requests.get("https://official-joke-api.appspot.com/random_joke")
 joke = response.json()
 print(f"Setup: {joke['setup']}")
 print(f"Punchline: {joke['punchline']}")
+
+print("This is the first version of the application")
